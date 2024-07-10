29e0eb732ef8be3338a9a334c2e29237e285a30b
Matias-F-Acevedo <matias.acevedo56732@gmail.com>
2024-07-10 18:25:28 -0300

    Method in the service to generate a regular student certificate

--- src/certificate/certificate.controller.ts
+++ src/certificate/certificate.controller.ts
@@ -13,15 +13,49 @@ import { Response } from 'express';
 export class CertificateController {
   constructor(private readonly certificateService: CertificateService) {}
 
-  @Get(':studentId/:examId')
-  async generateCertificate(
+  @Get('/assistance-exam/:studentId/:examId')
+  async generateCertificateExam(
     @Param('studentId') studentId: string,
     @Param('examId') examId: string,
     @Res() res: Response,
   ) {
     try {
       const filePath =
-        await this.certificateService.generateCertificate(+studentId,+examId);
+        await this.certificateService.generateCertificateExam(+studentId,+examId);
+
+      if (filePath instanceof HttpException) {
+        throw new HttpException(
+          'Certificate could not be generated',
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      }
+
+      res.sendFile(filePath, (err) => {
+        if (err) {
+          throw new HttpException('File not found', HttpStatus.NOT_FOUND);
+        }
+      });
+
+
+    } catch (error) {
+      throw new HttpException(
+        'Internal Server Error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+
+
+  @Get('/regular-student/:studentId/:examId')
+  async generateCertificateRegular(
+    @Param('studentId') studentId: string,
+    @Param('examId') examId: string,
+    @Res() res: Response,
+  ) {
+    try {
+      const filePath =
+        await this.certificateService.generateCertificateRegular(+studentId,+examId);
 
       if (filePath instanceof HttpException) {
         throw new HttpException(
